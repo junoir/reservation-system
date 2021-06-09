89e6a6781de7b2e6f2e8e3c915e8094be1c2a10d
Junoir <junoir.mklv@gmail.com>
2021-06-09 08:43:36 +0200

    added api endpoints

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Room } from  './room';
+import { Observable } from  'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  ROOMS_API_SERVER = "http://127.0.0.1:8080";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  // get all rooms
+  getRooms(): Observable<Room[]>{
+    return this.httpClient.get<Room[]>(`${this.ROOMS_API_SERVER}/api/read.php`);
+  }
+
+  // create a room
+  createRoom(room: Room): Observable<Room>{
+    return this.httpClient.post<Room>(`${this.ROOMS_API_SERVER}/api/create.php`, room);
+  }
+
+  // update a room
+  updateRoom(room: Room){
+    return this.httpClient.put<Room>(`${this.ROOMS_API_SERVER}/api/update.php`, room);   
+  }
+
+  // delete a room
+  deleteRoom(id: number){
+    return this.httpClient.delete<Room>(`${this.ROOMS_API_SERVER}/api/delete.php/?id=${id}`);
+  }
 }
