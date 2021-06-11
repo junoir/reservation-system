de0a3821ceff07501f1da1c5de69cb716b796bb6
Junoir <junoir.mklv@gmail.com>
2021-06-11 12:56:24 +0200

    npm i ng2-search-filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from  '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from  '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { ReservationsComponent } from './reservations/reservations.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    ReservationsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
+    MaterialModule,
+    Ng2SearchPipeModule,
     BrowserAnimationsModule
   ],
   providers: [],
